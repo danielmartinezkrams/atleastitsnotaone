7612e5b93cf41601e1a15c17748d205739139a42
Daniel Martinez-Krams <danielmartinezkrams@students.berkeley.net>
2018-05-11 10:05:51 -0700

    second

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
 import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
+import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to At Least It's not a One</h1>
-        </header>
-        <p className="App-intro">
-        </p>
-      </div>
+        <MuiThemeProvider>
+             <div className="App">
+                <header className="App-header">
+                  <img src={logo} className="App-logo" alt="logo" />
+                    <h1 className="App-title">Welcome to At Least It's not a One</h1>
+                 </header>
+                <p className="App-intro">
+                </p>
+            </div>
+        </MuiThemeProvider>
     );
   }
 }
