9c9b788c7ff9201eb3f6aca2a3e7774652d9cfc3
DanielMartinez-Krams <danielmartinezkrams@gmail.com>
2018-05-13 14:47:00 -0700

    menu

--- src/App.js
+++ src/App.js
@@ -2,29 +2,21 @@ import React, { Component } from 'react';
 import logo from './logo.png';
 import './App.css';
 import MuiThemeProvider from 'material-ui/styles/MuiThemeProvider';
+import IconMenu from 'material-ui/DropDownMenu';
+import MenuItem from 'material-ui/MenuItem';
+import { Switch, Route, Link } from 'react-router-dom';
+import IconButton from 'material-ui/IconButton';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
 
 class App extends Component {
     constructor(props) {
         super(props);
-
         this.state = {
             open: false,
         };
     }
-    handleClick = (event) => {
-        event.preventDefault();
 
-        this.setState({
-            open: true,
-            anchorEl: event.currentTarget,
-        });
-    };
-
-    handleRequestClose = () => {
-        this.setState({
-            open: false,
-        });
-    };
+    handleChange = (event, index, value) => this.setState({value});
 
     render() {
     return (
@@ -32,14 +24,64 @@ class App extends Component {
              <div className="App">
                 <header className="App-header">
                   <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to At Least It's not a One</h1>
+                    <h1 className="App-title">At Least It's not a One</h1>
+                    <div>
+                        <IconMenu iconButtonElement={<IconButton><MoreVertIcon /></IconButton>} anchorOrigin={{horizontal: 'left', vertical: 'top'}} targetOrigin={{horizontal: 'left', vertical: 'top'}}>
+                            <MenuItem primaryText="Home" containerElement={<Link to="/" />}/>
+                            <MenuItem primaryText="Page One" containerElement={<Link to="/pageOne" />}/>
+                            <MenuItem primaryText="Page Two" containerElement={<Link to="/pageTwo" />}/>
+                        </IconMenu>
+                    </div>
                  </header>
-                <p className="App-intro">
-                </p>
+                 <Main />
             </div>
         </MuiThemeProvider>
     );
   }
+}
+
+class Main extends Component {
+    render(){
+        return(
+            <Switch>
+                <div className="content">
+                    <Route exact path="/" component={Home}/>
+                    <Route path="/pageOne" component={pageOne}/>
+                    <Route path="/pageTwo" component={pageTwo}/>
+                </div>
+            </Switch>
+        )
+    }
+}
+
+class Home extends Component {
+    render(){
+        return(
+            <h1>
+                Home
+            </h1>
+        )
+    }
+}
+
+class pageOne extends Component {
+    render(){
+        return(
+            <h1>
+                Page One
+            </h1>
+        )
+    }
+}
+
+class pageTwo extends Component {
+    render(){
+        return(
+            <h1>
+                Page Two
+            </h1>
+        )
+    }
 }
 
 export default App;
