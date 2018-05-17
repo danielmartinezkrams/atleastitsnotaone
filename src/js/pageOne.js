139ae7fd77e8eaa584e09d495146c76a01b46488
Sarah Mosley <SarahMosley1@students.berkeley.net>
2018-05-17 10:00:35 -0700

    logoioa

--- src/js/pageOne.js
+++ src/js/pageOne.js
@@ -3,9 +3,13 @@ import React, { Component } from 'react';
 class pageOne extends Component {
     render(){
         return(
+            <div>
             <h1>
                 Page One
             </h1>
+                To request a meal delivery, please specify your desired meal. Another BHS student will go off campus to retrieve your lunch for you.
+                When they return, you will pay them the price of the meal plus $3.
+            </div>
         )
     }
 }
