6d0be3ae4322c5260adf26a7f8d23b6a621b146d
Sarah Mosley <SarahMosley1@students.berkeley.net>
2018-05-17 10:03:45 -0700

    logoioa

--- src/js/Home.js
+++ src/js/Home.js
@@ -8,7 +8,8 @@ class Home extends Component {
             <h1>
                 Home
             </h1>
-                Welcome to Berkeley Eats! Please first sign into your account or create a new one.
+                Welcome to Berkeley Eats! Here you can request another BHS student to pick up your lunch for you.
+                To begin, please first sign into your account or create a new one, and then select either "Place Order" or "Make Delivery."
             </div>
         )
     }
