4f945bbd522b21e612eea52ccdf9f94309227cf2
Daniel Martinez-Krams <danielmartinezkrams@students.berkeley.net>
2018-05-22 09:43:52 -0700

    page one

--- src/js/pageOne.js
+++ src/js/pageOne.js
@@ -8,16 +8,19 @@ class pageOne extends Component {
                 Page One
             </h1>
             <form>
-                    Resturaunt:<br>
-                    <input type="text" name="Choose Resturaunt"><br>
-                        Meal:<br>
-                        <input type="text" name="Choose Meal">
-                            Drink:<br>
-                            <input type="text" name="Choose Drink">
-                                Options:<br>
-                                <input type="text" name="Additional items. Please specify"/>
-                </form>
+                Resturaunt:<br />
+                <input type="text" name="Choose Resturaunt" />
+                <br />
+                Meal:
+                <br />
+                <input type="text" name="Choose Meal" />
+                Drink:
+                <br />
+                <input type="text" name="Choose Drink" />
+                Options:<br />
+                <input type="text" name="Additional items. Please specify"/>
                 <button type="button">Submit Order</button>
+            </form>
             </div>
         )
     }
