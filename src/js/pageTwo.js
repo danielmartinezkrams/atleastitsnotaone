93c0989a49cd297edd8b0240e721de436b0a7947
adrianfernandezkim <adrianfernandezkim@students.berkeley.net>
2018-05-23 10:07:45 -0700

    more grid list

--- src/js/pageTwo.js
+++ src/js/pageTwo.js
@@ -6,13 +6,18 @@ import Subheader from '@material-ui/core/ListSubheader';
 import IconButton from '@material-ui/core/IconButton';
 import InfoIcon from '@material-ui/icons/Info';
 import { Link } from 'react-router-dom';
-import suya from '../img/suya.png'
+import suya from '../img/suya.jpeg'
 import sliver from '../img/sliver.jpeg'
+import bongo from '../img/bongo.jpeg'
+import saigon from '../img/saigon.jpeg'
+
+
 
 class pageTwo extends Component {
 
     render(){
-        const tileData = [{img: suya, title: "Suya"}, {img: sliver, title:"Sliver"}, {}];
+        const tileData = [{img: suya, title: "Suya"}, {img: sliver, title:"Sliver"},{img: bongo, title:"Bongo Burger"},
+            {img:saigon , title:"Saigon Express" }, ];
         return(
             <div>
                 <div>
@@ -21,8 +26,8 @@ class pageTwo extends Component {
                             <Subheader component="div">Restaurants</Subheader>
                         </GridListTile>
                         {tileData.map(tile => (
-                            <Link to={`pageTwo/${tile.title}`}>
                             <GridListTile key={tile.img}>
+                                <Link to={`pageTwo/${tile.title}`}>
                                 <img src={tile.img} alt={tile.title} />
                                 <GridListTileBar
                                     title={tile.title}
@@ -32,8 +37,8 @@ class pageTwo extends Component {
                                         </IconButton>
                                     }
                                 />
+                                </Link>
                             </GridListTile>
-                            </Link>
                         ))}
                     </GridList>
                 </div>
