664e003a83cf7f99f3115d0bf0b2f408949f9076
Lorenzo Riutort <lorenzoriutort@students.berkeley.net>
2018-05-30 09:50:00 -0700

    color background

--- src/js/App.js
+++ src/js/App.js
@@ -21,7 +21,7 @@ const theme = createMuiTheme({
             light: '#ff7961',
             main: '#f44336',
             dark: '#ba000d',
-            contrastText: '#e89752',
+            contrastText: '#000',
         },
     },
 });
